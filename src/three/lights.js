80b9bc5e6ef38e696827dae6843eedbed957a9ab
Pratiyush Tiwari <aloomataloo@gmail.com>
2023-03-12 08:55:32 +0545

    Add doorlight

--- src/three/lights.js
+++ src/three/lights.js
@@ -1,10 +1,12 @@
-import { AmbientLight, DirectionalLight } from "three";
+import { AmbientLight, DirectionalLight, PointLight } from "three";
 import { gui, scene } from "./base";
+import { house } from './groups'
 
-const ambientLight = new AmbientLight('#ffffff', 0.5)
+
+const ambientLight = new AmbientLight('#b9d5ff', 0.12)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 
-const moonLight = new DirectionalLight('#ffffff', 0.5)
+const moonLight = new DirectionalLight('#b9d5ff', 0.12)
 moonLight.position.set(4, 5, - 2)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
@@ -12,5 +14,10 @@ gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
 
 
-scene.add(ambientLight)
-scene.add(moonLight)
+//DOORLIGHT
+const doorLight = new PointLight("#ff7d46", 1, 7)
+doorLight.position.set(0.2, 2.2, 2.7)
+house.add(doorLight)
+
+
+scene.add(ambientLight, moonLight)
