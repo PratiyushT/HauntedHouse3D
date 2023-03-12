33df236704d95ed34fedae7a85b5462af33cdd7c
Pratiyush Tiwari <aloomataloo@gmail.com>
2023-03-12 08:54:45 +0545

    Add 50 random graves

---
+++ src/three/groups.js
@@ -0,0 +1,4 @@
+import { Group } from "three";
+
+export const house = new Group();
+export const graves = new Group();
--- src/three/objects.js
+++ src/three/objects.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { BoxGeometry, ConeGeometry, DoubleSide, Group, Mesh, MeshStandardMaterial, PlaneGeometry, SphereGeometry } from 'three'
 import { scene } from './base'
+import { graves, house } from './groups'
 
 
 //FLOOR
@@ -46,27 +47,47 @@ const bushGeometry = new SphereGeometry(1, 16, 16)
 const bushMaterial = new MeshStandardMaterial({ color: "green" })
 
 const bush1 = new Mesh(bushGeometry, bushMaterial)
-bush1.scale.set(0.5,0.5,0.5)
-bush1.position.set(0.8,0.2,2.2)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
 
 const bush2 = new Mesh(bushGeometry, bushMaterial)
-bush2.scale.set(0.25,0.25,0.25)
-bush2.position.set(1.4,0.1,2.1)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
 
 const bush3 = new Mesh(bushGeometry, bushMaterial)
-bush3.scale.set(0.4,0.4,0.4)
-bush3.position.set(-0.8,0.1,2.2)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
 
 const bush4 = new Mesh(bushGeometry, bushMaterial)
-bush4.scale.set(0.15,0.15,0.15)
-bush4.position.set(-1,0.05,2.6)
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(-1, 0.05, 2.6)
 
 //HOUSE 
-const house = new Group();
 house.position.y = 0.01
 house.add(walls, roof, door)
 house.add(bush1, bush2, bush3, bush4)
 
-scene.add(house)
-scene.add(floor)
+
+//Grave
+
+const graveMaterial = new MeshStandardMaterial({ color: "grey" })
+const graveGemoetry = new BoxGeometry(0.4, 0.8, 0.1)
+
+for (let i = 0; i < 50; i++) {
+
+    const radius = 3 + Math.random() * 6
+    const angle = Math.random() * Math.PI * 2
+
+    const xAxis = Math.sin(angle) * radius
+    const zAxis = Math.cos(angle) * radius
+
+    const grave = new Mesh(graveGemoetry, graveMaterial)
+    grave.position.set(xAxis, 0.31, zAxis)
+    grave.rotation.y = (Math.random() - 0.5) * 0.6
+
+    graves.add(grave)
+}
+
+
+scene.add(house, floor, graves)
 
